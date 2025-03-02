2548925c85547ac77f0ac17a9e8c8b90f32fa991
Roman Mesler <meslerdev@gmail.com>
2025-03-02 20:28:39 +0500

    init

---
+++ src/test/kotlin/ru/aigineer/aigineer/AigineerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.aigineer.aigineer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AigineerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
