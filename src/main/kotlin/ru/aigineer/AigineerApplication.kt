21a6ba8ece6a382e5e0eebe048d16096061537fa
Roman Mesler <meslerdev@gmail.com>
2025-03-10 10:09:54 +0500

    init jdbc connect

--- src/main/kotlin/ru/aigineer/AigineerApplication.kt
+++ src/main/kotlin/ru/aigineer/AigineerApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class AigineerApplication
 
 fun main(args: Array<String>) {
-    runApplication<ru.aigineer.AigineerApplication>(*args)
+    runApplication<AigineerApplication>(*args)
 }
